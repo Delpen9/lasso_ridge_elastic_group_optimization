2b07de1aa7267e62613ccc2415d05e7d82821dd4
ian.dover <ian.dover@skunova.com>
2023-04-03 19:08:35 +0000

    spline coefficients graphed

--- Problem4/problem4.py
+++ Problem4/problem4.py
@@ -9,6 +9,36 @@ import matplotlib.pyplot as plt
 # Load the .mat file
 import scipy.io
 
+# B-Spline
+from scipy.interpolate import make_interp_spline, splrep, BSpline
+
+def spline_coefficients_graph(
+    X : np.ndarray,
+    filename : str,
+    sensor_number : int
+) -> None:
+    '''
+    '''
+    current_path = os.path.abspath(__file__)
+    file_path = os.path.abspath(os.path.join(current_path, '..', '..', 'output', filename))
+
+    color_palette = sns.color_palette('hls', 10)
+
+    for i in range(X.shape[0]):
+        sns.lineplot(
+            x = np.arange(0, X.shape[1]),
+            y = X[i],
+            color = color_palette[i % 10]
+        )
+
+    plt.title(fr'Sensor {str(sensor_number)}: Spline Coefficients')
+    plt.xlabel('Index')
+    plt.ylabel('Spline Coefficient')
+
+    plt.savefig(file_path, dpi = 100)
+    plt.clf()
+    plt.cla()
+
 def single_observation_graph(
     X : np.ndarray,
     filename : str,
@@ -49,12 +79,47 @@ def make_observation_graphs(
             sensor_number = sensor
         )
 
+def b_spline_single_row_reduction(
+    row : np.ndarray
+) -> np.ndarray:
+    '''
+    '''
+    knot_count = 8
+
+    knot_distance = row.shape[0] / (knot_count - 1)
+    knots = np.arange(0, row.shape[0] + knot_distance, knot_distance)
+
+    tck = splrep(np.arange(0, row.shape[0]), row, t = knots[1: -1])
+    spline = BSpline(*tck)
+    coefficients = spline.c[:-4]
+
+    return coefficients
+
 def b_spline_reduction(
+    sensor_data : np.ndarray
+) -> np.ndarray:
+    '''
+    '''
+    reduced_sensor_data = []
+    for row in sensor_data:
+        reduced_sensor_data.append(b_spline_single_row_reduction(row))
+    reduced_sensor_data = np.array(reduced_sensor_data)
+    return reduced_sensor_data
+
+def make_b_spline_observation_graphs(
     X : np.ndarray
 ) -> None:
     '''
     '''
-    return None
+    sensors = np.arange(0, 10)
+    for sensor in sensors:
+        reduced_sensor_data = b_spline_reduction(X[sensor].copy())
+
+        spline_coefficients_graph(
+            reduced_sensor_data,
+            filename = fr'reduced_sensor_{sensor}_observation',
+            sensor_number = sensor
+        )
 
 if __name__ == '__main__':
     current_path = os.path.abspath(__file__)
@@ -62,7 +127,8 @@ if __name__ == '__main__':
     data_train = scipy.io.loadmat(data_file_path)
 
     X_train = np.stack(data_train['x'][0])
-    make_observation_graphs(X_train)
+    # make_observation_graphs(X_train)
+    make_b_spline_observation_graphs(X_train)
 
     data_file_path = os.path.abspath(os.path.join(current_path, '..', '..', 'data', 'NSC.test.mat'))
     data_test = scipy.io.loadmat(data_file_path)
